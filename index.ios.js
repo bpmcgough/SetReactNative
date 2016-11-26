96e72d31f5a0dac3840cf90563981f40e0e485dc
Brian McGough <bpmcgough@gmail.com>
2016-11-25 18:52:45 -0800

    first branch commit

--- index.ios.js
+++ index.ios.js
@@ -50,7 +50,7 @@ handleFoundSet = () => {
 
 generateCards();
 
-class Button extends Component {
+class Card extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -62,80 +62,69 @@ class Button extends Component {
        width: 90,
        height: 100,
        borderWidth: 1,
-       backgroundColor: this.props.card.selected ? '#CCC' : 'white'
+       backgroundColor: this.props.card.backgroundColor
       }
     };
   }
 
-  handleChange() {
-    (this.props.handlePress.bind(this))(this.props.card);
-    let style = _.extend({}, this.state.style);
-    style.backgroundColor = (style.backgroundColor === 'white' ? '#CCC' : 'white');
-    this.setState({style});
-  }
-
   render() {
-    if(this.props.card){
-      return (
-        <TouchableHighlight style={this.state.style} onPress={this.handleChange.bind(this)}>
-          <Image
-            source={this.props.card.img} resizeMode='center' style={styles.image}
-          />
-        </TouchableHighlight>
-      );
-    } else {
-      return (
-        <TouchableHighlight style={this.state.style}>
-          <Text>End of the deck!</Text>
-        </TouchableHighlight>
-      )
-    }
+    return (
+      <TouchableHighlight style={this.state.style}>
+        <Image
+          source={this.props.card.img} resizeMode='center' style={this.state.style}
+        />
+      </TouchableHighlight>
+    )
   }
 }
 
-let cardsArray;
+// so here's the goal:
+// show nothing at first
+// make button that, when pressed, displays a card
+// make another button that, when pressed, changes the cards background color
 
 class SetProject extends Component {
+
   constructor(props) {
     super(props);
-  }
-
-  handlePress(card){
-    if(card.selected === false){
-      card.selected = true;
-      selectedCards.push(card);
-    } else {
-      card.selected = false;
-      selectedCards = selectedCards.filter( el => el !== card);
-    }
-    if(selectedCards.length === 3 && Helpers.checkForSet(selectedCards)){
-      handleFoundSet(selectedCards);
-      this.forceUpdate();
+    this.state = {
+      cardArray: [{backgroundColor: 'green', color: 'red', selected: false, number: 1, fill: 'empty', shape: 'oval', img: require('/Users/michaelmcgough/Desktop/Coding/SideProjects/SetProject/cardImages/ovals/1-red-empty-oval.png'), id: 1}]
     }
   }
 
-  generateCards(){
-    generateCards();
+  displayNewCard(){
+    this.state.cardArray = [];
+    this.state.cardArray.push(<Card card={this.state.cardArray[0]}></Card>);
     this.forceUpdate();
   }
 
-  makeCardsArray(){
-    cardsArray = currentCards.map((card) => {
-      return (
-        <Button card={card} handlePress={this.handlePress.bind(this)} backgroundColor='white'></Button>
-      )
-    });
+  changeBackgroundColor(){
+    this.state.cardArray = [];
+    let newCardArray = [{backgroundColor: 'white', color: 'red', selected: false, number: 1, fill: 'empty', shape: 'oval', img: require('/Users/michaelmcgough/Desktop/Coding/SideProjects/SetProject/cardImages/diamonds/1-red-empty-diamond.png'), id: 1, key: 1}]
+    // {backgroundColor: 'white', color: 'red', selected: false, number: 1, fill: 'empty', shape: 'oval', img: require('/Users/michaelmcgough/Desktop/Coding/SideProjects/SetProject/cardImages/diamonds/1-red-empty-diamond.png'), id: 1, key: 1}}
+    this.setState({cardArray: newCardArray});
+    console.log('well?', this.state.cardArray);
   }
 
+  // was previously inside style: <Card card={this.state.card1} changeBackgroundColor={this.changeBackgroundColor}></Card>
+
   render() {
-    this.makeCardsArray();
+    let cardsArray = this.state.cardArray.map(theCard => {
+      return (
+        <Card card={theCard}></Card>
+      );
+    });
+
     return (
       <View style={styles.overarch}>
         <View style={styles.container}>
           {cardsArray}
         </View>
-        <TouchableHighlight onPress={this.generateCards.bind(this)} style={styles.submit}>
-          <Text style={styles.welcome}>Start Over</Text>
+        <TouchableHighlight style={styles.image}>
+          <Text onPress={this.displayNewCard.bind(this)}>Display A Card</Text>
+        </TouchableHighlight>
+        <TouchableHighlight>
+          <Text onPress={this.changeBackgroundColor.bind(this)}>Change Background</Text>
         </TouchableHighlight>
       </View>
     );
@@ -157,7 +146,7 @@ const styles = StyleSheet.create({
     marginBottom: 10,
     justifyContent: 'center',
     alignItems: 'center',
-    flexWrap: 'wrap',
+    // flexWrap: 'wrap',
     flexDirection: 'column'
   },
   container: {
@@ -175,8 +164,206 @@ const styles = StyleSheet.create({
   image: {
     width: 50,
     height: 70
+  },
+  card: {
+   flex: 1,
+   justifyContent: 'center',
+   alignItems: 'center',
+   margin: 5,
+   width: 90,
+   height: 100,
+   borderWidth: 1,
+   backgroundColor: "white"
   }
 });
 
 
 AppRegistry.registerComponent('SetProject', () => SetProject);
+
+// import React, { Component } from 'react';
+// import {
+//   AppRegistry,
+//   StyleSheet,
+//   Text,
+//   View,
+//   TouchableHighlight,
+//   ListView,
+//   Image
+// } from 'react-native';
+// import _ from 'underscore';
+// import Helpers from './theDirectory/helpers';
+// import Cards from './theDirectory/allTheCards';
+//
+// let allTheCards = Cards;
+// let selectedCards = [];
+// let usedCards = [];
+// let currentCards = [];
+//
+// shuffle = Helpers.shuffle;
+// checkForSet = Helpers.checkForSet;
+//
+// generateCards = () => {
+//   allTheCards = Helpers.shuffle(allTheCards);
+//   currentCards = [];
+//   while(currentCards.length < 12){
+//     currentCards.push(allTheCards.pop());
+//   }
+// };
+//
+// handleFoundSet = () => {
+//   currentCards = currentCards.filter(el => {
+//     let shouldReturn = true;
+//     selectedCards.forEach(card => {
+//       if(card.id === el.id){
+//         shouldReturn = false;
+//       }
+//     });
+//     if(shouldReturn) return el;
+// 	});
+//
+//   usedCards = usedCards.concat(selectedCards);
+//
+//   selectedCards = [];
+//
+//   for(let i = 0; i < 3; i++){
+//     currentCards.push(allTheCards.pop());
+//   }
+// };
+//
+// generateCards();
+//
+// class Button extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       style: {
+//        flex: 1,
+//        justifyContent: 'center',
+//        alignItems: 'center',
+//        margin: 5,
+//        width: 90,
+//        height: 100,
+//        borderWidth: 1,
+//        backgroundColor: "white"
+//       }
+//     };
+//   }
+//
+//   componentDidMount(){
+//     let style = _.extend({}, this.state.style);
+//     style.backgroundColor = 'orange';
+//     this.setState({style});
+//   }
+//
+//   handleChange() {
+//     (this.props.handlePress.bind(this))(this.props.card);
+//     let style = _.extend({}, this.state.style);
+//     style.backgroundColor = (style.backgroundColor === 'white' ? '#CCC' : 'white');
+//     this.setState({style});
+//   }
+//
+//   render() {
+//     if(this.props.card){
+//       return (
+//         <TouchableHighlight style={this.state.style} onPress={this.handleChange.bind(this)}>
+//           <Image
+//             source={this.props.card.img} resizeMode='center' style={styles.image}
+//           />
+//         </TouchableHighlight>
+//       );
+//     } else {
+//       return (
+//         <View></View>
+//       )
+//     }
+//   }
+// }
+//
+// let cardsArray;
+//
+// class SetProject extends Component {
+//   constructor(props) {
+//     super(props);
+//   }
+//
+//   handlePress(card){
+//     if(card.selected === false){
+//       card.selected = true;
+//       selectedCards.push(card);
+//     } else {
+//       card.selected = false;
+//       selectedCards = selectedCards.filter( el => el !== card);
+//     }
+//     if(selectedCards.length === 3 && Helpers.checkForSet(selectedCards)){
+//       handleFoundSet(selectedCards);
+//       this.forceUpdate();
+//     }
+//   }
+//
+//   generateCards(){
+//     generateCards();
+//     this.makeCardsArray();
+//     this.forceUpdate();
+//   }
+//
+//   makeCardsArray(){
+//     cardsArray = currentCards.map((card) => {
+//       return (
+//         <Button card={card} handlePress={this.handlePress.bind(this)} backgroundColor='white'></Button>
+//       )
+//     });
+//   }
+//
+//   render() {
+//     this.makeCardsArray();
+//     return (
+//       <View style={styles.overarch}>
+//         <View style={styles.container}>
+//           {cardsArray}
+//         </View>
+//         <TouchableHighlight onPress={this.generateCards.bind(this)} style={styles.submit}>
+//           <Text style={styles.welcome}>Shit Fuck</Text>
+//         </TouchableHighlight>
+//       </View>
+//     );
+//   }
+// }
+//
+// const styles = StyleSheet.create({
+//   title: {
+//     flex: .2,
+//     justifyContent: 'center'
+//   },
+//   submit: {
+//     flex: .2,
+//     justifyContent: 'center',
+//   },
+//   overarch: {
+//     flex: 1,
+//     marginTop: 25,
+//     marginBottom: 10,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     flexWrap: 'wrap',
+//     flexDirection: 'column'
+//   },
+//   container: {
+//     flex: 1,
+//     justifyContent: 'space-between',
+//     alignItems: 'center',
+//     flexWrap: 'wrap',
+//     flexDirection: 'column'
+//   },
+//   welcome: {
+//     fontSize: 20,
+//     textAlign: 'center',
+//     margin: 10,
+//   },
+//   image: {
+//     width: 50,
+//     height: 70
+//   }
+// });
+//
+//
+// AppRegistry.registerComponent('SetProject', () => SetProject);
